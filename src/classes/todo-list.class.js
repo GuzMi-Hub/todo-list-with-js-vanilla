44fe961cf59fa7b862bd121dd3a30dcf1611ed69
Erick Fernando Guzmán Valerio <guzmi.dev@gmail.com>
2021-09-25 13:55:00 -0600

    :zap: todo class and todolist class created

---
+++ src/classes/todo-list.class.js
@@ -0,0 +1,15 @@
+export class TodoList {
+  constructor() {
+    this.todos = [];
+  }
+
+  nuevoTodo(todo) {
+    this.todo.push(todo);
+  }
+
+  eliminarTodo(id) {}
+
+  toogleTodo(id) {}
+
+  eliminarCompletadors() {}
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./styles.css";
+import { Todo, TodoList } from "./classes";
+
+const todoList = new TodoList();
+
+const tarea = new Todo("Aprender Javascript");
+
+console.log(tarea);
